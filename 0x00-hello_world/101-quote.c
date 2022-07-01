85cc75066bc06b7b3fd1074d8c78a58806d22ed3
Nalon03 <nalongrace03@gmail.com>
2022-07-01 03:40:01 -0700

    Task 8 still fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-char quote[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, quote, sizeof(quote) -1);
-return (1);
+}	
+        char quote[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        write(2, quote, sizeof(quote) -1);
+        return (1);
 }

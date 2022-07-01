dc910aa8247d40b10a22e2ff7df25d62e9431ff2
Nalon03 <nalongrace03@gmail.com>
2022-07-01 03:45:48 -0700

    Ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 }	
 char quote[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, quote, sizeof(quote) -1);
-    return (1);
+return (1);
 }

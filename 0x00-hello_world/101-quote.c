5857f9d4dc502b2a301e150f6878433188175f40
Nalon03 <nalongrace03@gmail.com>
2022-07-01 04:11:00 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -7,8 +7,8 @@
  * Return: Always 1 (Success)
  */
 int main(void)
+
 }	
-char quote[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, quote, sizeof(quote) -1);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }

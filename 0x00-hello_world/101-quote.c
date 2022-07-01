7090615428bf6280457272de5027cec6c27888b5
Nalon03 <nalongrace03@gmail.com>
2022-07-01 03:26:50 -0700

    Task 8 fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
 int main(void)
 
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+char quote[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+write(2, quote, sizeof(quote) -1);
 return (1);
 }

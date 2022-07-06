169a15dd7858b8094f22f059efae3161c3d390e4
Nalon03 <nalongrace03@gmail.com>
2022-07-06 04:58:33 -0700

    Task 2 fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-char c, i;
-for (i = 0; i <= 9; i++)
-{
-for (c = 'a'; c <= 'z'; c++)
-{	
-_putchar(c);
+	char c, i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
-_putchar('\n');
-}
-}
+

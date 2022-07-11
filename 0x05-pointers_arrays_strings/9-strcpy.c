cb9d2ee8ca1c52c64cb4cc642a4996090db1f439
Nalon03 <nalongrace03@gmail.com>
2022-07-11 08:25:07 -0700

    Task 8 Still fixing

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	for (; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (i);
+int i = 0;
+for (; src[i] != '\0'; i++)
+{
+	dest[i] = src[i];
+}
+return (dest);
 }

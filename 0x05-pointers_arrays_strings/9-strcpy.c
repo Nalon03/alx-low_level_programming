01f38dede2ff939d35bbd265bde4208660b9adf5
Nalon03 <nalongrace03@gmail.com>
2022-07-15 13:07:35 -0700

    Task fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * char *_strcpy - a function that copies the string pointed to by src
@@ -8,10 +8,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i = 0;
-for (; src[i] != '\0'; i++)
-{
-	dest[i] = src[i];
-}
-return (dest);
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }

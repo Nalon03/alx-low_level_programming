add9613e69b98abe97a2b0ed3adb0623926de202
Nalon03 <nalongrace03@gmail.com>
2022-07-18 05:57:52 -0700

    Task 0 fixing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #define HEADER_FILE
 
-int _putchar(char c);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);

069bb36527699f8f3260c5dab000400c468350bf
Nalon03 <nalongrace03@gmail.com>
2022-07-22 09:22:49 -0700

    Task 0

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,36 @@
+/*
+ * File: 2-strncpy.c
+ * Auth: Nalon Grace
+ */
+
+#include "main.h"
+
+/**
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+P
---
+++ 0x09-static_libraries/vi
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+int _atoi(char)
+{
+
+	return (0);
+}

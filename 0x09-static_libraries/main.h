14c6156743c0ee9a34667449b509b49f59583783
Nalon03 <nalongrace03@gmail.com>
2022-07-27 05:55:31 -0700

    Task 0 fixing

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -5,7 +8,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);
+int _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -18,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif

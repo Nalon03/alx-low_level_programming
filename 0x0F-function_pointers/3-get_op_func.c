b8cee6cf91ed06fca1fc0bb3f3490d8f7044ff0e
Nalon03 <nalongrace03@gmail.com>
2022-08-03 10:50:41 -0700

    Task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,90 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
- * op_add - Adds two numbers @a and @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Sum of @a and @b
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
  */
-
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-/**
- * op_sub - Calculates the difference of @c and @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Difference of @a and @b
- */
-
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
- * op_mul - Multiplies two numbers
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Product of @a and @b
- */
-
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - Divides @a by @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Result of the division of @a by @b without remainder
- */
-
-int op_div(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a / b);
-}
-
-/**
- * op_mod - Calculates the remainder of @a divided by @b
- *
- * @a: A number
- *
- * @b: A number
- *
- * Return: Remainder of @a divider by @b
- */
-
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a % b);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * @argc: length of command line arguments
- *
- * @argv: double pointer to cli arguments
- *
- * Return: 0
- */
-
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
 int main(int argc, char *argv[])
 {
-	int (*func_ptr)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -22,21 +19,21 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	func_ptr = get_op_func(argv[2]);
-
-	if (func_ptr == NULL)
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-
-	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
-
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
